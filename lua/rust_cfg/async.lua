c58f2ff4e82c0a279aefdd03cc448388b46981ea
Jannik Peters <jannik@nlnetlabs.nl>
2024-11-08 15:38:36 +0100

    refactor: remove unused functions

--- lua/rust_cfg/async.lua
+++ lua/rust_cfg/async.lua
@@ -15,44 +15,9 @@ function M.launch(f, ...)
 end
 
 function M.wrap(f)
-   return function(...)
-      M.launch(f, ...)
-   end
-end
-
-
-function M.throttle(f, timeout)
-   local last_call = 0;
-
-   local timer = nil
-
-   return function(...)
-
-      if timer then
-         timer:stop()
-      end
-
-      local rem = timeout - (vim.loop.now() - last_call)
-
-      if rem > 0 then
-
-         if timer == nil then
-            timer = vim.loop.new_timer()
-         end
-
-         local args = { ... }
-         timer:start(rem, 0, vim.schedule_wrap(function()
-            timer:stop()
-            timer:close()
-            timer = nil
-            last_call = vim.loop.now()
-            f(unpack(args))
-         end))
-      else
-         last_call = vim.loop.now()
-         f(...)
-      end
-   end
+  return function(...)
+    M.launch(f, ...)
+  end
 end
 
 return M
